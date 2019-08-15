4764c0e66c1c68aa615318d475482d79061c7a53
Alexi Most <alexibmost@gmail.com>
2019-08-15 16:32:37 -0700

    materials

---
+++ ComputerGraphics/Assets/MaterialModifier.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialModifier : MonoBehaviour {
+
+    public float cycleTime;
+    public Color materialColor;
+    public List<Texture> textures;
+
+    Material thisMaterial;
+
+    float elapsedCycle;
+    int textureIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        thisMaterial = GetComponent<Renderer>().material;
+        OnValidate();
+
+        elapsedCycle = 0;
+        textureIndex = 0;
+    }
+
+    void Update()
+    {
+        elapsedCycle += Time.deltaTime;
+        if(elapsedCycle > cycleTime)
+        {
+            elapsedCycle = 0;
+            textureIndex++;
+            if(textureIndex >= textures.Count)
+            {
+                textureIndex = 0;
+            }
+            thisMaterial.mainTexture = textures[textureIndex];
+        }
+    }
+
+    void OnValidate()
+    {
+        if(null != thisMaterial)
+        {
+            thisMaterial.color = materialColor;
+        }
+    }
+}
