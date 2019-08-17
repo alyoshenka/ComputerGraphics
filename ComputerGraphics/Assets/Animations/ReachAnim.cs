ae1f6abc11eb63cb1b0cb6d2374a76c9079d74ce
Alexi Most <alexibmost@gmail.com>
2019-08-16 17:59:25 -0700

    fix commit

---
+++ ComputerGraphics/Assets/Animations/ReachAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachAnim : MonoBehaviour
+{
+    public Transform lookTransform;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (animator)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, lookTransform.position);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, lookTransform.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, lookTransform.rotation);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, lookTransform.rotation);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(lookTransform.position, 0.1f);
+    }
+}
---
+++ ComputerGraphics/Assets/Scripts/CustomShapes/CustomMesh.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class CustomMesh : MonoBehaviour
+{
+
+    protected Mesh customMesh; // store so it can be destroyed
+    protected Vector3[] corners = new Vector3[0];
+
+    void OnDestroy()
+    {
+        if (null != customMesh)
+        {
+            Destroy(customMesh);
+        }
+    }
+
+
+    protected void OnDrawGizmos()
+    {
+        foreach (Vector3 v in corners)
+        {
+            Gizmos.DrawSphere(transform.position + v, 0.1f);
+        }
+    }
+}
