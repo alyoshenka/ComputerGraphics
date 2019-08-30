b9769f6120b46da5b7f7c4594c877597c5397950
Alexi Most <alexibmost@gmail.com>
2019-08-29 17:48:10 -0700

    shaders broke

--- renderer/renderer/main.cpp
+++ renderer/renderer/main.cpp
@@ -11,6 +11,7 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 int main() 
 {
@@ -27,6 +28,7 @@ int main()
 	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, true);
 #endif
 
+	// custom geometry
 	// CCW Triangle
 	vertex triVerts[] =
 	{
@@ -46,22 +48,25 @@ int main()
 	unsigned int triIndices[] = { 0, 1, 2};
 	unsigned int quadIndices[] = { 3, 2, 0, 3, 1, 2 };
 
+	// create geometry
 	geometry triangle = makeGeometry(triVerts, 3, triIndices, 3);
-	// geometry quad = makeGeometry(quadVerts, 4, quadIndices, 6);
-	// geometry customQuad = makePlane(1.5f, 0.8f);
-	// geometry obj = loadObj("Geometry/tri.obj");
+	geometry quad = makeGeometry(quadVerts, 4, quadIndices, 6);
+	geometry customQuad = makePlane(1.5f, 0.8f);
 	geometry cust = loadObj("Geometry/teapot.obj");
 
+	// load shaders
 	shader basicShad = makeShader(load("Shaders/basicVert.txt").c_str(), load("Shaders/basicFrag.txt").c_str());
 	shader colorShad = makeShader(load("Shaders/colorVert.txt").c_str(), load("Shaders/colorFrag.txt").c_str());
 	shader camShad = makeShader(load("Shaders/camVert.txt").c_str(), load("Shaders/camFrag.txt").c_str());
 	shader uvShad = makeShader(load("Shaders/uvVert.txt").c_str(), load("Shaders/uvFrag.txt").c_str());
 	shader lightShad = makeShader(load("Shaders/lightVert.txt").c_str(), load("Shaders/lightFrag.txt").c_str());
 
+	// set up camera
 	glm::mat4 triModel = glm::identity<glm::mat4>();
 	glm::mat4 camProj = glm::perspective(glm::radians(45.f), 640.f / 480.f, 0.1f, 100.f);
 	glm::mat4 camView = glm::lookAt(glm::vec3(0, 0, -40), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
+	// lad textures
 	texture tex = loadTexture("Assets/soulspear_diffuse.tga");
 	texture tex2 = loadTexture("Assets/x.png");
 	texture tex3 = loadTexture("Assets/splat.png");
@@ -71,9 +76,14 @@ int main()
 	sun.dir = glm::vec4{ 0, 0, 1, 1 };
 	sun.col = glm::vec4{ 1, 0, 1, 1 };
 
+	// set shader uniforms
 	setUniform(camShad, 0, tex, 0);
 	setUniform(camShad, 1, tex2, 1);
 	setUniform(camShad, 2, tex3, 2);
+
+	setUniform(camShad, 0, camProj);
+	setUniform(camShad, 1, camView);
+	setUniform(camShad, 2, triModel);
 
 	setUniform(lightShad, 0, camProj);
 	setUniform(lightShad, 1, camView);
@@ -82,15 +92,6 @@ int main()
 	setUniform(lightShad, 3, marble, 0);
 	setUniform(lightShad, 4, sun.dir);
 	setUniform(lightShad, 5, sun.col);
-
-	float angle = 0;
-	float scaleNumber = 200;
-	float scaleCount = 0;
-	float scaleValue = 0.01;
-	float scaleCurrent = 0.995;
-
-	float lerp = 0;
-	float dir = 0.01f;	
 
 	// glfw
 	GLdouble x = 0;
@@ -101,54 +102,36 @@ int main()
 	float xVal;
 	float yVal;
 
+	// enable cycling
+	std::vector<geometry> geos;
+	std::vector<shader> shads;
+
+	geos.push_back(triangle);
+	geos.push_back(quad);
+	geos.push_back(customQuad);
+	geos.push_back(cust);
+
+	shads.push_back(basicShad);
+	shads.push_back(colorShad);
+	// shads.push_back(camShad);
+	shads.push_back(uvShad);
+	shads.push_back(lightShad);
+
+	int geoIdx = 0;
+	int shadIdx = 0;
+	int frameWait = 50;
+	int frameElapsed = 50;
+
 	while (!game.shouldClose())
 	{
 		game.tick();
 		game.clear();
 
-		/*t.tick();
-		std::cout << t.deltaTime() << std::endl;
-		*/
-
-		// triModel = glm::translate(triModel, glm::vec3(cos(angle) / 20, sin(angle) / 20, 0));
-		// angle += 0.1f;
-
+		// move camera
 		triModel = glm::rotate(triModel, glm::radians(1.f), glm::vec3(0, 1, 0));
-
-		/*triModel = glm::scale(triModel, glm::vec3(scaleCurrent, 1, 1));
-		scaleCount++;
-		if (scaleCount > scaleNumber)
-		{
-			scaleCount = 0;
-			if (scaleCurrent > 1)
-			{
-				scaleCurrent = 1 - scaleValue;
-			}
-			else
-			{
-				scaleCurrent = 1 + scaleValue;
-			}
-		}*/
-		
-		/*
-		setUniform(camShad, 0, camProj);
-		setUniform(camShad, 1, camView);
-		*/
-		// setUniform(camShad, 2, triModel);
-
-		/*
-		setUniform(lightShad, 0, camProj);
-		setUniform(lightShad, 1, camView);
-		*/
 		setUniform(lightShad, 2, triModel);
 
-		 // draw(camShad, quad);
-		 // draw(colorShad, triangle);
-		 // draw(lightShad, triangle);
-		 // draw(basicShad, customQuad);
-
-		// draw(lightShad, triangle);
-
+		// move directional light
 		glfwGetCursorPos(game.window, &x, &y);
 		xDif = invlerp(0, w, x);
 		yDif = invlerp(0, h, y);
@@ -156,31 +139,43 @@ int main()
 		yVal = mylerp(-shadowMax, shadowMax, yDif);
 		sun.dir.x = xVal;
 		sun.dir.y = yVal;
-
 		setUniform(lightShad, 4, sun.dir);
 
-		draw(lightShad, cust);
-
-		// assert(glGetError() == GL_NO_ERROR);
-
-		/*GLint loc = glGetUniformLocation(uvShad.program, "val");
-		if (loc != -1)
+		// check for changes
+		int state = glfwGetMouseButton(game.window, GLFW_MOUSE_BUTTON_LEFT);
+		if (frameElapsed > frameWait && state == GLFW_PRESS)
 		{
-			lerp += 0.001f;
-			glUniform1f(loc, (float)sin(lerp));
-			std::cout << loc << std::endl;
+			geoIdx++;
+			frameElapsed = 0;
+			if (geoIdx >= geos.size())
+			{
+				geoIdx = 0;
+			}
 		}
-		else
+		state = glfwGetMouseButton(game.window, GLFW_MOUSE_BUTTON_RIGHT);
+		if (frameElapsed > frameWait && state == GLFW_PRESS)
 		{
-			std::cout << loc << std::endl;
+			shadIdx++;
+			frameElapsed = 0;
+			if (shadIdx >= shads.size())
+			{
+				shadIdx = 0;
+			}
 		}
+		frameElapsed++;
 
-		draw(uvShad, triangle);*/
+		// draw(shads[shadIdx], geos[geoIdx]);
+
+		// draw(camShad, triangle);
+		draw(colorShad, triangle);
+		// draw(lightShad, triangle);
+		// draw(basicShad, customQuad);
+	    // draw(lightShad, triangle);
 	}
 
 	freeGeometry(triangle);
-	// freeGeometry(quad);
-	// freeGeometry(customQuad);
+	freeGeometry(quad);
+	freeGeometry(customQuad);
 	freeShader(basicShad);
 	freeShader(colorShad);
 	freeShader(camShad);
