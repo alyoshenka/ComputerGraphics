60e448884557d8b02c129c2959da0558bd1e93ad
Alexi Most <alexibmost@gmail.com>
2019-09-13 17:29:39 -0700

    hlsl shader works

--- ComputerGraphics/Assets/_Assessment/CustomPostProcess.cs
+++ ComputerGraphics/Assets/_Assessment/CustomPostProcess.cs
@@ -10,6 +10,14 @@ public sealed class CustomPostProcess : PostProcessEffectSettings
 {
     [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
     public FloatParameter blend = new FloatParameter { value = 0.5f };
+
+    [Range(-0.01f, 0.01f)]
+    public FloatParameter offsetX = new FloatParameter { value = 0 };
+    [Range(-0.01f, 0.01f)]
+    public FloatParameter offsetY = new FloatParameter { value = 0 };
+    [Range(-0.01f, 0.01f)]
+    public FloatParameter offsetZ = new FloatParameter { value = 0 };
+
 }
 
 public sealed class CustomPostProcessRenderer : PostProcessEffectRenderer<CustomPostProcess>
@@ -18,6 +26,11 @@ public sealed class CustomPostProcessRenderer : PostProcessEffectRenderer<Custom
     {
         var sheet = context.propertySheets.Get(Shader.Find("PostProcess/Test1"));
         sheet.properties.SetFloat("_Blend", settings.blend);
+
+        sheet.properties.SetFloat("_OffsetX", settings.offsetX);
+        sheet.properties.SetFloat("_OffsetY", settings.offsetY);
+        sheet.properties.SetFloat("_OffsetZ", settings.offsetZ);
+
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
