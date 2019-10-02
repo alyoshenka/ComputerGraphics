8e81817765261b7b22add57c42278477473601a4
Alexi Most <alexibmost@gmail.com>
2019-10-02 11:55:15 -0700

    invert postprocessing

---
+++ ComputerGraphics/Assets/Shaders/FullscreenPostprocessing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FullscreenPostprocessing : MonoBehaviour
+{
+    [SerializeField]
+    private Material postprocessMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, postprocessMaterial);
+    }
+}
